098211ad3aeb908797f2f2397cd3c4786f60b5fb
uthmanduro <udurosinlohun@gmail.com>
2022-12-11 16:08:54 -0800

    comb done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for num1 in range(0, 100):
-    if num1 in range(0, 10):
-        print("0", end="")
     if num1 == 99:
         print(num1)
-    print("{}, ".format(num1), end="")
+    else:
+        print("{0:02d}, ".format(num1), end="")
