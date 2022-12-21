695d531ff26d1fcc03f9c0d9e6480623375fccda
uthmanduro <udurosinlohun@gmail.com>
2022-12-20 17:38:59 -0800

    raise value error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """ Initialize an instance
+        Args:
+            size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
