37c276a6454127e2f9127395469bf597b16704f9
uthmanduro <udurosinlohun@gmail.com>
2022-12-27 10:26:56 -0800

    2d list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    copy = []
     for i in matrix:
         for j in i:
-            print("{}".format(j), end=" ")
-        print("{}".format("\n"))
+            print("{:d}".format(j), end= " ")
+        print()
