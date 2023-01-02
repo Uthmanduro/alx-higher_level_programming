7d1c7fefc9dc999cd8dab23f8b293f60964c620a
uthmanduro <udurosinlohun@gmail.com>
2023-01-02 15:56:19 -0800

    fix type error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,20 +12,6 @@ class Rectangle:
         self.__height = height
 
     @property
-    def height(self):
-        """return the value of height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """sets the value of height"""
-        if not isinstance(value, int):
-            raise TypeError("height must me an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
     def width(self):
         """reutrns the value of width"""
         return self.__width
@@ -34,7 +20,21 @@ class Rectangle:
     def width(self, value):
         """sets the value of width"""
         if not isinstance(value, int):
-            raise TypeError("width must me an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """return the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
