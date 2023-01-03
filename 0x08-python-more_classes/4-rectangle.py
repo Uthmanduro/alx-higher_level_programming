2d38dc898917daff0b230fdfe98c294616749937
uthmanduro <udurosinlohun@gmail.com>
2023-01-02 17:37:02 -0800

    __repr

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Defines a module class"""
+
+
+class Rectangle:
+    """class rectangle created"""
+    def __init__(self, width=0, height=0):
+        """initialize the instance variable"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """gets the value of the width"""
+        return self.__width
+
+    def width(self, value):
+        """sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """gets the value of the height"""
+        return self.__height
+
+    def height(self, value):
+        """sets the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """print the string representation of #"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        string = ""
+        for item in range(self.height):
+            for column in range(self.width):
+                string += "#"
+            if item != self.height - 1:
+                string += "\n"
+        return string
+
+    def __repr__(self):
+        """return string representation of rectangle"""
+        x = "Rectangle(" + str(self.width) + ', ' + str(self.height) + ')'
+        return x
