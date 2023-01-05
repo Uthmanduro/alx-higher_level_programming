75f7682bc25c38bf06aa03d09fb268bcb8690ae1
uthmanduro <udurosinlohun@gmail.com>
2023-01-04 16:13:28 -0800

    check

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,24 +18,28 @@ class Rectangle:
         """gets the value of the width"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         """sets the value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
         """gets the value of the height"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         """sets the value of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """returns the area of the rectangle"""
@@ -84,4 +88,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        width = height = size
+        new_instance = cls(width, height)
+        return new_instance
