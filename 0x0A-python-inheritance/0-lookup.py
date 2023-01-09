ff6e3d493c6c4bd9c7db486714759bc2a007478b
uthmanduro <udurosinlohun@gmail.com>
2023-01-09 15:13:09 -0800

    module documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a functioon thats returns the list of availablel attributes and methods"""
+
+
 def lookup(obj):
     """
         returns the list of available attributes and methods of an object
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a function that checks whether the onject is the same class"""
+
+
 def is_same_class(obj, a_class):
     """returns true if object is an instance of  the specifed class
         else false
