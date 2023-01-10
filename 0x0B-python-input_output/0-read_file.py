8c1bb4d20ec5c6b83296d456ef26a7c7af2b3f77
uthmanduro <udurosinlohun@gmail.com>
2023-01-10 14:47:55 -0800

    read and write

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Defines a duntion that reads a text file and prints it to standard output
+"""
+
+
+def read_file(filename=""):
+    """reads a file"""
+    with open(filename, encoding="utf-8") as myfile:
+        print(myfile.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    writes a string to a text file and returns the number of characters written
+"""
+def write_file(filename="", text=""):
+    """writes a string to a file"""
+    with open(filename, mode="w", encoding="utf-8") as myfile:
+        number_written = myfile.write(text)
+        return(number_written)
