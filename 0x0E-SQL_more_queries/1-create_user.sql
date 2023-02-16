bc0c9f77248b45a231a67eccf506b965550912ae
uthmanduro <udurosinlohun@gmail.com>
2023-02-15 18:01:31 -0800

    1 to 8

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a new mysql server iser_0d_1 and has all priviledges
-CREATE USER IF NOT EXISTS user_0d_1
+CREATE USER IF NOT EXISTS user_0d_1@localhost
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1;
+GRANT ALL ON *.* TO user_0d_1@localhost;
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- lists all cities of california that is found in database
+SELECT * FROM states cities
+WHERE name = 'california'
+ORDER BY cities.id ASC;
