6f7d6e1a43a63e6b55af6271113a8768b34e91da
uthmanduro <udurosinlohun@gmail.com>
2023-02-15 16:43:44 -0800

    trial

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- lists all privilieges of the mysql users on the server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates a new mysql server iser_0d_1 and has all priviledges
+CREATE USER IF NOT EXISTS user_0d_1
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1;
