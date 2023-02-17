015b7041290566f61ad1d008078d5eeb21185c8d
uthmanduro <udurosinlohun@gmail.com>
2023-02-16 19:02:18 -0800

    over and out

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all cities of california that is found in database
-SELECT *
+SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'california')
 ORDER BY id ASC;
