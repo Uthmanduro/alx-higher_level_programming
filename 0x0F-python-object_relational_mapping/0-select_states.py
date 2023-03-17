fb11309b0083ecbed840d4cd5fe7d8b151d49195
uthmanduro <udurosinlohun@gmail.com>
2023-03-17 04:08:09 -0700

    try task 1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists al states from the database"""
+import MySQLdb
+import sys
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                     passwd=password, database=dbname)
+cur = db.cursor()
+with open('0-select_states.sql', 'r') as filename:
+    my_script = filename.read()
+cur.execute(my_script)
+cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+table = cur.fetchall()
+for item in table:
+    print(item)
+db.close()
