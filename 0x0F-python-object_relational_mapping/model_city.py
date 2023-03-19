7626ebc328c9ebee54ff9b0a22d68297ee4117ad
uthmanduro <udurosinlohun@gmail.com>
2023-03-19 13:07:24 -0700

    task 15

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains the class definition of a city"""
+
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from model_state import Base, State
+from sqlalchemy.orm import relationship
+import sys
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', backref='cities')
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contains the class definition of a state and an instance Base"""
+
+
+from sqlalchemy import create_engine, Table, Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """inherits from base and links to the mysql table 'states'"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
