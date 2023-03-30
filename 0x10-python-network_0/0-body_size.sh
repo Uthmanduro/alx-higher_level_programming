82acdb466c1e9a39b8f58bb0cb791b8ee30be998
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-03-30 16:34:46 -0700

    fixed shell check

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #takes in a url, sends a request to the url and displays the size of the body
-curl -i -s $1 | awk '/Content-Length/ {print $2}'
+curl -i -s "$1" | awk '/Content-Length/ {print $2}'
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #send a delete request to the url and displays the body
-curl -X DELETE $1
+curl -X DELETE "$1"
