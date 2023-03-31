45b3e5ebfcf88ee75ba37411694293f592da33ae
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-03-31 16:03:39 -0700

    task 8 search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,14 +6,16 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1 or type(sys.argv) == str:
-        q = ""
+    if len(sys.argv) == 1 or type(sys.argv[1]) != str:
+        param = ""
     else:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': sys.argv[1]})
-        print(response.status_code)
-        if response.json() == None:
+        param = sys.argv[1]
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user',
+                                 data={'q': param})
+        if response.json() == {}:
             print("No result")
-        elif len(response.json()) > 1:
-            print(f"[{response.json()['id']}] {response.json()['name']}")
         else:
-            print("Not a valid JSON")
+            print(f"[{response.json()['id']}] {response.json()['name']}")
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
