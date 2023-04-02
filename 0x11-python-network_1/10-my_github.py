e1431e4cf997a392a5f22825fae5d0748d3cbcc8
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-02 07:51:58 -0700

    fetch user from github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,8 +6,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    token = sys.argv[2]
     headers = {"X-GitHub-Api-Version": "2022-11-28",
-               "Authorization":
-               "Bearer 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"}
-    response = requests.get("https://api.github.com/user", headers=headers)
-    print(response.json()['id'])
+               "Authorization": f"{token}",
+               "Accept": "application/vnd.github+json"}
+    response = requests.get("https://api.github.com/user", headers=headers,
+                            auth=(sys.argv[1], sys.argv[2]))
+    print(response.json().get('id'))
