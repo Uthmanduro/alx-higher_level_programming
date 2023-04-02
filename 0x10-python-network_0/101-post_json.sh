ac4c55a2d89b32afd4e66bdb1be7b5b8144ef9df
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-02 16:49:40 -0700

    fixed bug in advanced task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends json post request with contnets of a file and displays body of respons
-curl -sv -X POST -H "Content-Type: application/json" --data-binary "@$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" --data-binary "@$2" "$1"
