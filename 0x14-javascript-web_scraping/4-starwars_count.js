7bc37247ecf86118d3d9f9e7c074e3a3bdfed7ee
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-25 23:26:36 +0000

    request store

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,11 +8,9 @@ request.get(url, function (err, res, data) {
   const response = JSON.parse(data);
   let count = 0;
   for (const i of response.results) {
-    if (i.characters) {
-      for (const chars of i.characters) {
-        if (chars === 'https://swapi-api.alx-tools.com/api/people/18/') {
-          count++;
-        }
+    for (const chars of i.characters) {
+      if (chars === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count++;
       }
     }
   }
