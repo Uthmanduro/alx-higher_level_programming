409486bb0bd67588811134b7bd6e3bf4a4c1997e
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-04-26 02:34:23 +0000

    asyc/await

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
 // prints all characters of a starwars movie
 const request = require('request');
+const { promisify } = require('util');
+const promisifiedRequest = promisify(request);
+
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id + '/';
-request.get(url, function (err, response, data) {
-  if (err) throw err;
-  const body = JSON.parse(data);
-  const chars = body.characters;
-  for (let links = 0; links < chars.length; links++) {
-    const newurl = chars[links];
-    request.get(newurl, function (err, response, data) {
-      if (err) throw err;
-      const newbody = JSON.parse(data);
+
+(async function() {
+  try {
+    const body = await promisifiedRequest(url);
+    const chars = JSON.parse(body.body);
+    for (const links of chars.characters) {
+      const newreq = await promisifiedRequest(links);
+      const newbody = JSON.parse(newreq.body);
       console.log(newbody.name);
-    });
+    }
+  } catch (err) {
+    console.error(err);
   }
-});
+})();
